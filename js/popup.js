a3b7fd219f0cd3bff83f085ded7dd7b6f059a315
eu-tsareva <eu.tsareva@gmail.com>
2017-01-27 13:54:59 +0300

    merge branches, rename files

---
+++ js/popup.js
@@ -0,0 +1,28 @@
+var feedbackPopup = document.querySelector('.modal-content-feedback');
+
+var contactButton = document.querySelector('.contacts-btn');
+contactButton.addEventListener('click',
+  function(event) {
+    event.preventDefault();
+    feedbackPopup.classList.add('modal-content-show');
+    feedbackPopup.querySelector('.form-field').focus();
+  });
+
+var closePopupButtons = document.getElementsByClassName('modal-content-close');
+for (var i = 0; i < closePopupButtons.length; i++) {
+  closePopupButtons[i].addEventListener('click',
+    function(event) {
+      event.preventDefault();
+      this.parentNode.classList.remove('modal-content-show');
+    }
+  );
+}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    var openedPopup = document.querySelector('.modal-content-show');
+    if (openedPopup) {
+      openedPopup.classList.remove("modal-content-show");
+    }
+  }
+});
